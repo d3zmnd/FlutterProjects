f726bd8ece955b266df8faea76a1415108406191
Vitaly Davidenko <35844879+d3zmnd@users.noreply.github.com>
2019-11-05 14:52:15 +0200

    Add files via upload

---
+++ codelab_4/lib/components/weather_app.dart
@@ -0,0 +1,108 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Weather {
+  Weather(this.temperature, this.locationName, this.iconUrl);
+  Weather.fromJson(Map<String, dynamic> json)
+      : temperature = json['main']['temp'],
+        locationName = json['name'],
+        iconUrl = _generateIconUrl(json['weather'][0]['icon']);
+
+  final int temperature;
+  final String locationName;
+  final String iconUrl;
+  static String _generateIconUrl(String icon) =>'https://openweathermap.org/img/wn/$icon@2x.png';
+}
+class WeatherProvider {
+Future<Weather> getCurrentWeather() async {
+  final http.Response response = await http.get(
+      'https://api.openweathermap.org/data/2.5/weather?q=Kharkiv&units=metric&APPID=1ea55013049215603ece3fee22806975');
+    if (response.statusCode == 200) {
+      return Weather.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+}
+class WeatherContainer extends StatelessWidget {
+  const WeatherContainer({Key key, @required this.weather})
+      : assert(weather != null),
+        super(key: key);
+  final Weather weather;
+
+  @override
+  Widget build(BuildContext context) {
+    return OrientationBuilder(
+      builder: (BuildContext context, Orientation orientation) => 
+      orientation == Orientation.portrait ? portrait(context) : landscape(context));
+  }
+  Widget portrait(BuildContext context){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          '${weather.locationName}  ${weather.temperature} °C',
+          style: Theme.of(context).textTheme.display1,
+          textAlign: TextAlign.center,
+        ),
+        Image.network(weather.iconUrl),
+      ],
+    );
+  }
+  Widget landscape(BuildContext context){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '${weather.locationName}',
+              style: Theme.of(context).textTheme.display1,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              '${weather.temperature} °C',
+              style: Theme.of(context).textTheme.display1,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(weather.iconUrl),
+          ],
+        ),
+      ],  
+    );
+  }
+}
+class WeatherOverview extends StatelessWidget {
+final WeatherProvider _weatherProvider = WeatherProvider();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather')
+      ),
+      body: Center(
+        child: FutureBuilder<Weather>(
+          future: _weatherProvider.getCurrentWeather(),
+          builder: (BuildContext context, AsyncSnapshot<Weather> snapshot) {
+            if (snapshot.hasData) {
+              return WeatherContainer(weather: snapshot.data);
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
---
+++ codelab_4/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'components/weather_app.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.red,        
+        brightness: Brightness.dark,   
+      ),
+      home: WeatherOverview(),
+    );
+  }
+}
